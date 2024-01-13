0e64b21af05ce9da5f8f23b07f303ad531a78f07
Passamon S <88275001+qpsss@users.noreply.github.com>
2024-01-13 21:59:28 +0700

    Initial commit

---
+++ include/dbt/target/compiled/retail/models/report/report_product_invoices.sql
@@ -0,0 +1,10 @@
+SELECT
+  p.product_id,
+  p.stock_code,
+  p.description,
+  SUM(fi.quantity) AS total_quantity_sold
+FROM `passamons-retail-data-pipeline`.`retail`.`fct_invoices` fi
+JOIN `passamons-retail-data-pipeline`.`retail`.`dim_product` p ON fi.product_id = p.product_id
+GROUP BY p.product_id, p.stock_code, p.description
+ORDER BY total_quantity_sold DESC
+LIMIT 10
